b7f27f8647fa523fe0bc8b2adf480fcba2245166
Milana Berhe <mjber@visitor-10-56-99-35.wifi.berkeley.edu>
2025-02-07 13:41:53 -0800

    added hwmk 1

---
+++ homework1.py
@@ -0,0 +1,12 @@
+#1 pwd - print working directory (current location in system); -L prints logical path
+#1 cd - change directory; -n takes you back to home directory
+#1 ls - print files/directions in current directory; -a shows ALL files, including hidden ones
+#1 mkdir - make new directory; -v print message for eahc created directory
+#1 cp - copy file; cp -r copies an entire directory to a specific one
+#1 mv - move file; -n no overwrite
+#2.1 Git is the language, GH is the inetrnet remote repository, Git Bash is an emulator
+#2.2 the terminal is where we implement the code and the command line is how to use python from a terminal
+#2.3 windows powercell is a different langauge used for windows devices and git Bash is used in Mac
+#2.4 python is a language and Anaconda is what we is to install and use python (Conda is how Anaconda manages packages)
+#2.5 Jupyter notebook let's you make and share documents that could contain code. Jupyter Lab is another program that can be used on top of Jupyter Notebook for more advanced computing
+#2.6 VS Code is a system that let's you move between programming languages and debug
