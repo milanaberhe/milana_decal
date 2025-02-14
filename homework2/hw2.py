2da2c9711e993f1f75bc3cd6d9e5eb93c2192f87
Milana Berhe <mjber@wifi-10-40-97-49.wifi.berkeley.edu>
2025-02-14 13:17:52 -0800

    adding hwmk 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,20 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
-# 2) What does ~/ mean?
-# 3) What's the difference between and abolute and a relative path in your terminal?
-# 4) What command would bring you back to your home directory?
+#cd ..
+# 2) What does ~/ mean? 
+# Home directory 
+# 3) What's the difference between and abolute and a relative path in your terminal? 
+# Relative path might take you through other directories while absolute path will not
+# 4) What command would bring you back to your home directory? 
+# cd
 # 5) If you called rm ./ in your current working directory, what would it do? 
-# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# It removes entries for specificed files within a directory
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"? 
+# "git add" adds a change in the working directory to the staging area; "git commit" svaes the staged changes; "git push" uploads the local repository to a remote one
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# it tells you what's in the staging area and what you're about to push
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
-# 9) How does cloning a repository differ from pulling from a repository?
-# 10) Tell me a fun fact!
+# I haven't really encoundered anything severe so far, but there's a lot to remember and I've struggled with that a bit - I feel like I have to look at my notes all the time and I don't retain anything I read anyway.
+# 9) How does cloning a repository differ from pulling from a repository? 
+# It'll make a copy in a different repository and pulling will move it 
+# 10) Tell me a fun fact! 
+# I speak russian!
